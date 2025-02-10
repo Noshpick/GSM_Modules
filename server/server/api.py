3a836791585b8a84101a727ff2362f7fc84d11d3
nOshpa <lilsadqq@yandex.ru>
2025-02-11 09:00:55 +1000

    Corse
    Add corse

--- server/server/api.py
+++ server/server/api.py
@@ -9,6 +9,15 @@ modem_manager.connect()
 
 class BaseHandler(tornado.web.RequestHandler):
     
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE, PUT")
+        self.set_header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+    def options(self, *args, **kwargs):
+        self.set_status(204)
+        self.finish()
+
     def get_available_ports(self):
         return list(modem_manager.modems.keys())
 
