da7a7e35809f7339684db8d419602c448b38fe70
nOshpa <lilsadqq@yandex.ru>
2025-03-04 20:05:57 +1000

    test

--- server/main.py
+++ server/main.py
@@ -1,10 +1,14 @@
 from server.api import make_app
-import tornado.ioloop
 from server.models import init_db
+import tornado.ioloop
+import time
 
-print("Инициализация базы данных...")
+print("🚀 Инициализация базы данных...")
 init_db()
-print("База данных инициализирована!")
+print("✅ База данных инициализирована!")
+
+print("⌛ Ожидание 3 секунды перед запуском сервера...")
+time.sleep(3)
 
 if __name__ == "__main__":
     app = make_app()
